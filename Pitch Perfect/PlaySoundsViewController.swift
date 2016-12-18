cf0ea752a86396c8c2e459b85536a3fbf208323e
Sujal Ghosh <sujal.ghosh@gmail.com>
2016-12-18 22:31:04 +0530

    Initial commit.

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Sujal Ghosh on 18/12/16.
+//  Copyright © 2016 Sujal. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+}
---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Sujal Ghosh on 18/12/16.
+//  Copyright © 2016 Sujal. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundsViewController: UIViewController {
+    
+}
